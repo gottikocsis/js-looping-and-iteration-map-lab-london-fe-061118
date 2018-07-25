6a0d394639c67582d7da68995078fd755b9d27f7
kogotti@yahoo.com <kogotti@yahoo.com>
2018-07-25 18:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(){
-  for (const student of students) {
-  patroni.push( student.namePatronus() );
+  for (const drivers of drivers) {
+  newDrivers.push( student.to() );
 }
 }
